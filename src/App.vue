d279db4c681c3c729230b3a1adebd70cb8246956
MrGians <gianlumura97@gmail.com>
2022-07-25 16:57:58 +0200

    Header Links data moved to assets>data js

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <TheHeader />
+    <TheHeader :links="headerLinks" />
     <MainContent :current-series="currentSeries" :banner-items="bannerItems" />
     <TheFooter />
   </div>
@@ -8,7 +8,7 @@
 
 <script>
 // Data
-import { currentSeries, bannerItems } from "./assets/data/index.js";
+import { currentSeries, bannerItems, headerLinks } from "./assets/data/index.js";
 // Components
 import TheHeader from "./components/TheHeader.vue";
 import MainContent from "./components/MainContent.vue";
@@ -21,6 +21,7 @@ export default {
     return {
       currentSeries,
       bannerItems,
+      headerLinks,
     };
   },
 };
--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -1,4 +1,5 @@
 import currentSeries from "./_dc-comics.json";
 import bannerItems from "./_banner-items.js";
+import headerLinks from "./_header-links";
 
-export { currentSeries, bannerItems };
+export { currentSeries, bannerItems, headerLinks };
