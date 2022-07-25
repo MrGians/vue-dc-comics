e8dbffd9babd88f1446be22fdbe67f013b053962
MrGians <gianlumura97@gmail.com>
2022-07-25 15:05:22 +0200

    imported json data

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@
 </template>
 
 <script>
+// Data
+import currentSeries from "./assets/data/dc-comics.json";
+// Components
 import TheHeader from "./components/TheHeader.vue";
 import MainContent from "./components/MainContent.vue";
 import TheFooter from "./components/TheFooter.vue";
@@ -14,6 +17,11 @@ import TheFooter from "./components/TheFooter.vue";
 export default {
   name: "App",
   components: { TheHeader, MainContent, TheFooter },
+  data() {
+    return {
+      currentSeries,
+    };
+  },
 };
 </script>
 
